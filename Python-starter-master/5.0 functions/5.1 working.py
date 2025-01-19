b58a40693c4bcb24acb1b001e397dbba53bcb028
TenengFaith <.muluhtenengfaithjunior@gmail.com>
2025-01-19 17:41:28 +0100

    todo list app

--- Python-starter-master/5.0 functions/5.1 working.py
+++ Python-starter-master/5.0 functions/5.1 working.py
@@ -23,4 +23,9 @@ def is_prime():
 # function for name age school department and generate story
 def band_name(name,age,school,department):
     print(f"My name is {name}, i am {age} years old and i school in {school} in the department of {department}")
-band_name(name="faith",age=18,school="NAHPI",department="COMPUTER ENGINEERING")
+#band_name(name="faith",age=18,school="NAHPI",department="COMPUTER ENGINEERING")
+name=input("enter your name: ")
+age=input("enter your age: ")
+school=input("enter your school: ")
+department=input("enter your department: ")
+band_name(name,age,school,department)
---
+++ Python-starter-master/5.0 functions/todo.py
@@ -0,0 +1,46 @@
+# we need a list alias empty bucket
+todo_list=[]
+#       FIRST FUNCTION
+#adding to the list
+def add_task():
+    item=input("enter a task: ")        # task to be added
+    todo_list.append(item)      # add taks to the list
+    print("task successfully added😊")
+#       SECOND FUNCTION
+#deleting a task
+def delete_task():
+    if len(todo_list)==0:
+        print("there is no task in the list to be deleted😢")
+    else:  # enumerate 
+        print("    TASKS💀   \n")
+        for index, item in enumerate(todo_list):
+           print(f"{index+1}. {item}")
+    item_index=int(input("enter the item number to be deleted💣: "))-1
+    if item_index<0 or item_index>=len(todo_list):
+        print("out of range❌")
+    else:
+        todo_list.remove(item_index) # or del todo_list[item_index]
+        read_list()
+
+
+#           THIRD FUNCTION
+#updating list: changing item
+def update_list():
+    if len(todo_list)==0:
+        print("there is no task in the list to be updated😢")
+    else:  # enumerate 
+        print("    TASKS💀   \n")
+        for index, item in enumerate(todo_list):
+           print(f"{index+1}. {item}")
+    update_index=int(input("enter the item number to be updated⭕: "))-1
+    if update_index<0 or update_index>=len(todo_list):
+        print("out of range❌")
+    else:
+        new_item=input("enter the new task: ")
+        read_list()
+
+
+#           FOURTH FUNCTION
+def read_list():
+    for index,item in enumerate(todo_list):
+        print(f"{index}. {item}")
