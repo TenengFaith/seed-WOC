f0616f50f137aacab4d9d323c237bedd48a71253
TenengFaith <.muluhtenengfaithjunior@gmail.com>
2025-01-19 16:15:10 +0100

    more functions

--- Python-starter-master/5.0 functions/5.1 working.py
+++ Python-starter-master/5.0 functions/5.1 working.py
@@ -6,4 +6,21 @@ def odd_sum():
             sum+=i
         i+=1
     print(sum)
-odd_sum()
+#odd_sum()
+
+def is_prime():
+    number=int(input("enter a number to check if it is a prime number: "))
+    for i in range(2,number-1):
+        if number%i==0:
+            print(f"{number} is not a prime number")
+            break
+    else:
+        print(f"{number} is a prime number")
+#is_prime()
+
+
+#                               FUNCTIONS WITH PARAMETERS
+# function for name age school department and generate story
+def band_name(name,age,school,department):
+    print(f"My name is {name}, i am {age} years old and i school in {school} in the department of {department}")
+band_name(name="faith",age=18,school="NAHPI",department="COMPUTER ENGINEERING")
